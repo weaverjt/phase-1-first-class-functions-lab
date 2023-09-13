b148fb1f094fc1e220cbee6f2fe54b3fe79f4767
Jacob <weaver.jacob.t@gmail.com>
2023-09-12 20:15:00 -0500

    easy peasy

--- index.js
+++ index.js
@@ -1,1 +1,38 @@
 // Code your solution in this file!
+
+function returnFirstTwoDrivers(x = []) {
+    const firstTwo = x.slice(0, 2);
+    return firstTwo;
+}
+
+function returnLastTwoDrivers(y = []) {
+    const lastTwo = y.slice(2);
+    return lastTwo;
+
+}
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+
+function createFareMultiplier (mult) {
+    function timesFare (fare) {
+        return fare * mult;
+
+    }
+    return timesFare;
+}
+
+function fareDoubler(fare) {
+    return fare * 2;
+}
+
+function fareTripler(fare) {
+    return fare * 3;
+}
+
+
+
+function selectDifferentDrivers(arrayOfDrivers, xy) {
+   return xy(arrayOfDrivers); 
+    
+
+} 
+
